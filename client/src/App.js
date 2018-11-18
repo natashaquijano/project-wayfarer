fc69fb96aa02102b96a62e9fa406e3bc1ab6d382
atfcreative <atfcreative@gmail.com>
2018-11-17 23:59:01 -0800

    adds front-end homepage with working functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Homepage from './components/pages/homepage/Homepage';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
