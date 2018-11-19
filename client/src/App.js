c8905506d423ec03c1e3bb8281df103bf1badd38
Natasha Quijano <niquijanodesign@gmail.com>
2018-11-18 17:07:10 -0800

    deleted package-lock.json

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { isAuthenticated } from './helper/'
 
 import Home from './pages/Home';
 import Login from './pages/Login';
-import SignUp from './pages/SignUp'
+import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 
 
